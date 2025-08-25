2e2054d99096369d617ea60fa13d4152601c86c6
Weird-CAl <calvintanuri515@gmail.com>
2025-08-25 20:35:59 +0700

    add main index page

---
+++ pro70crazy/index.php
@@ -0,0 +1,110 @@
+<!--XHTML info goes here including the HTML tag-->
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html 
+xmlns="http://www.w3.org/1999/xhtml"
+xml:lang="en">
+<!--end of XHTML info-->
+  <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5VSJQ8K');</script>
+<!-- End Google Tag Manager -->
+    <title>The Pro 70 Official Website!</title>
+    <!-- Based on the website using Carrd -->
+    <!--<base href="https://pro70crazy.carrd.co">-->
+    <!-- Used CSS file -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Website icons -->
+    <link rel="icon" type="image/x-icon" href="images/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <!-- Manifest used for website icons -->
+    <link rel="manifest" href="/site.webmanifest">
+    <!-- end of icons database list -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Website for developers. Visit the HTMLtest page for testing features.">
+    <meta name="keywords" content="Pro 70, Pro 70 Crazy">
+    <meta name="author" content="Calvin B. Tanuri">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z2P7D5WKJK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z2P7D5WKJK');
+</script>
+    <meta name="google-site-verification" content="p14PLaLhqsR9npz6azxMINNr70z3GRr7eK4gbJUsHv4" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8215058597079261"
+     crossorigin="anonymous"></script>
+    <script>
+      console.log("REMEMBER!!!");
+      console.log("Inspect Element only applies to yourself!!!!!");
+      console.log("SO DON'T TRY TO HACK WITH INSPECT ELEMENT!!!!!!");
+      console.log("This website was made using Repl.It. To view the complete source code, you can go to https://replit.com/@Pro70CraZy or view it on GitHub with the link https://github.com/Pro70Crazy/HTMLtest.");
+      console.log("ENJOY!!!!!");
+    </script>
+    <noscript>This website requires JavaScript. Please enable it to keep this website work properly...</noscript>
+  </head>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VSJQ8K"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <!--<script> if (location.pathname == "/"){ var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { document.body.innerHTML = xhttp.responseText; } }; xhttp.open("GET", "/error/404.html", true); xhttp.send();} } </script>-->
+    <marquee width="90%" direction="left" height="20px" scrollamount=20>NEW VIDEOS COMING SOON!!!!!!!! Welcome to Pro 70! You'll find Entertaining Content at Pro 70! Subscribe to Pro 70! Since 2015! Watch Now on YouTube: Pro 70 Crazy!!!!!!!</marquee>
+    <h1>Welcome to Pro 70!</h1>
+    <p>Pro 70 is an entertainment network since 2015.</p> <br> <br>
+    <br><br>
+    <!--<h3>Best Random Videos</h3>
+    <iframe width="560" height="301" src="https://www.youtube.com/embed/W7R9J13ceWE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CANq2uU8SjA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/IPZO85HFM4w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/T0Ek7Od7TUY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/gdcLw-TE3eg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br><br>
+    <br><br>
+    <iframe src="/HTMLtest/scrolling_text.html" width="400" height="200" name=iframe title="this is a porn site hehe"></iframe><br>-->
+    <a href="https://tag.rc24.xyz/749830333553180727"><img src="https://tag.rc24.xyz/749830333553180727/tag.png" alt="RiiTag" /></a> <br>
+    <h3>Pages</h3> <br>
+    <!--All links to some website or pages provided below (outdated please ignore this comment)-->
+    <a href="about.html">About</a> <br>
+    <a href="coming_soon.html">Shop</a> <br>
+    <a href="questions">Questions</a> <br>
+    <a href="coming_soon.html">Commisions</a> <br>
+    <a href="faq.html">FAQ</a> <br>
+    <a href="/diary/">Calvin's Diary</a> <br>
+    <a href="coming_soon.html">Boardfess</a> <br>
+    <a href="/HTMLtest/">Test HTML, CSS, PHP and JavaScript Features</a> <br>
+    <br>
+    <h3>Links</h3>
+    <a href="https://pro70crazy.carrd.co">2nd Website</a> <br>
+    <a href="https://pro70crazy.blogspot.com">Calvin's Blog</a> <br>
+    <a href="https://www.twitter.com/pro70_crazy">Twitter</a> <br>
+    <a href="https://linktr.ee/pro70thesussycrazy">LinkTree</a> <br>
+    <a href="https://twitch.tv/arcade_70">Twitch</a> <br>
+    <a href="https://www.youtube.com/channel/UCXWlgaCBAWXlc81nHySyDTA?sub_confirmation=1">YouTube</a> <br>
+    <a href="https://luxuriousquixoticloop.fred311o.repl.co">Frederic's Website</a> <br>
+    <a href="https://patreon.com/pro70_crazy">Donate</a> <br>
+    <a href="https://www.soundcloud.com/pro170">SoundCloud</a> <br>
+    <a href="https://gaming-at-my.best/QC42UY">Pro 70 Polls</a> <br>
+    <a href="https://discord.gg/SrGJhjbqaY">Discord</a> <br>
+    <a href="https://tag.rc24.xyz/749830333553180727">RiiTag</a> <br>
+    <!--Spaces for the footer-->
+<br><br>
+<br><br>
+
+<!--Footer used for Copyright-->
+    <div class="copyright">
+  <a href="/contact.html">Contact Us</a>
+    <p>Copyright &copy;2023 Pro 70</p>
+  </div>
+  </body>
+</html>
